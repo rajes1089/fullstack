663c40e2633269d7b8751ff430efbc0064ab6503
Rajes <d.rajeswaran@gmail.com>
2016-06-03 15:25:02 +0530

    'hybrid'

---
+++ public_html/Angular/week4a_Yo/conFusion/app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name conFusionApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the conFusionApp
+ */
+angular.module('conFusionApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ public_html/Angular/week4a_Yo/conFusion/app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name conFusionApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the conFusionApp
+ */
+angular.module('conFusionApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
